60535eb0c92da3b6e9bd4282619a6e588d91a80d
Marisa Chow <mlchow@users.noreply.github.com>
2015-02-07 19:51:33 -0500

    Update popup.js
    copying updated popup.js. Use this instead of test.js

--- popup.js
+++ popup.js
@@ -1,127 +1,66 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- **/
 
 
+function highlight(query) {
+    console.log("highlight");
+    console.log(query);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {query: query}, function(response) {
+        
+        // Do Something with the Response
+        if (response.success)
+          console.log("Success!");
+        else
+          console.log("Failure!");
 
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+        console.log("Number?: " + response.number);
+      
+      });
     });
-  });
-});
-
-window.addEventListener('click',function(e){
-  if(e.target.href!==undefined){
-    chrome.tabs.create({url:e.target.href})
   }
-})
+
+
+// findTags() will find and highlight the search term tag in the given
+// source code. 
+function findTags() {
+  var search = document.getElementById( "search" );
+  highlight(search.value);
+}
+
+// learnMore() opens a new tab to the w3schools URL appropriate to 
+// the search term. 
+// in the format: http://www.w3schools.com/tags/tag_[name].asp 
+function learnMore() {
+  var search = document.getElementById( "search" );
+  var div = document.body.children[0];
+  var span = document.createElement('span');
+  span.innerHTML = search.value;
+  resetTerm(span);
+
+  var s1 = "http://www.w3schools.com/tags/tag_";
+  var s2 = span.innerHTML;
+  var s3 = ".asp";
+  var url = s1.concat(s2.concat(s3));
+  window.open(url);
+}
+
+// getTerm() determines the appropriate URL value and changes the 
+// span's innerHTML.
+// eg: <!--...--> will change the innerHTML to 'comment'
+function resetTerm(span) {
+  var item = span.innerHTML;
+  if (item == "!--...--" || item == "<!--...-->") 
+    span.innerHTML = "comment";
+  if (item == "h1" || 
+    item == "h2" ||
+    item == "h3" ||
+    item == "h4" ||
+    item == "h5" ||
+    item == "h6")
+    span.innerHTML = "hn";
+}
+
+var button = document.getElementById( "search-button" ); //store variable from .html doc, ID=search, into variable 'search'
+button.addEventListener("click", findTags);
+
+var learnmore = document.getElementById( "learn-more" );
+learnmore.addEventListener("click", learnMore);
