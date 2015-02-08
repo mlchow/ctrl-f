1b0d1812e5683199f55025735c09bd20aa97a81b
Elizabeth Bradley <erb2@princeton.edu>
2015-02-08 00:51:05 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,26 +1,129 @@
+var isRed    = false;
+var isYellow = false;
+var isBlue   = false;
+var isGreen  = false;
+
+var red    = "rgb(255,107,107)";
+var yellow = "rgb(255,174,107)";
+var blue   = "rgb(78,186,186)";
+var green  = "rgb(92.221,92)";
+
+var red_id    = "";
+var yellow_id = "";
+var blue_id   = "";
+var green_id  = "";
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    
-    var onList = true;
+	
+    var tagName = request.query;
+    var color = request.col;
 
-	var tagName = request.query;
+    /* Highlight the given tab */
+    if (request.task == "highlight") {
+        var num = 0;
 
-	var num = 0;
+        /* Unhighlight if a color is already selected */
+        if (color == "red") {
+            if (isRed) {
+                $(red_id).each(function() {
+                    $(this).css("background-color", "");
+                });
+                
+            }
+            $(tagName).each(function() {
+                $(this).css("background-color", red);
+                num++;
+            });
 
-    $(tagName).each(function() {
-    	$(this).css("color", "yellow");
-    	num++;
-    });
-    
-    if (onList) {
-    	sendResponse({
-    		success: true,
-    		number: num
-    	});
+            isRed = true;
+            red_id = tagName;
+        }
+
+        else if (color == "green") {
+             if (isGreen)  {
+                $(green_id).each(function() {
+                    $(this).css("background-color", "");
+                });
+                
+            }
+            $(tagName).each(function() {
+                $(this).css("background-color", green);
+                num++;
+                $(this).scrollIntoView();
+            });
+
+            isGreen = true;
+            green_id = tagName;
+
+        }
+
+        else if (color == "blue") {
+            if (isBlue)  {
+                $(blue_id).each(function() {
+                    $(this).css("background-color", "");
+                });
+                
+            }
+            $(tagName).each(function() {
+                $(this).css("background-color", blue);
+                num++;
+            });
+
+            isBlue = true;
+            blue_id = tagName;
+        }
+
+        else if (color == "yellow") {
+            if (isYellow)  {
+                $(yellow_id).each(function() {
+                    $(this).css("background-color", "");
+                });
+                
+            }
+            $(tagName).each(function() {
+                $(this).css("background-color", yellow);
+                num++;
+            });
+
+            isYellow = true;
+            yellow_id = tagName;
+        }
+
+
+        // $(tagName).each(function() {
+        // 	$(this).css("background-color", "#FFAE6B");
+        // 	num++;
+        // });
+
+        sendResponse({number: num});
     }
-    else
-    	sendResponse({
-    		success: false,
-    		number: 0
-    	});
-  });
+
+    /* Unhighlight the given tab */
+    else {
+        if (tagName == red_id) {
+            red_id = "";
+            isRed = false;
+        }
+        else if (tagName == yellow_id) {
+            yellow_id = "";
+            isYellow = false;
+        }
+        else if (tagName == blue_id) {
+            blue_id = "";
+            isBlue = false;
+        }
+        else if (tagName == green_id) {
+            green_id = "";
+            isGreen = false;
+        }
+
+        $(tagName).each(function() {
+            $(this).css("background-color", "");
+        });      
+
+        sendResponse({number: -1});
+    }
+
+    
+});
